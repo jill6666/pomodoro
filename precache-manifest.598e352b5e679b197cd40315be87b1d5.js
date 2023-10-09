cd96bb5107cfc16d8e9923dbb737a58145355a12
jillchang <jill.chang@jkos.com>
2023-10-09 18:49:11 +0800

    Updates

---
+++ precache-manifest.598e352b5e679b197cd40315be87b1d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5452ca3b198b6a0ad7d9473be75842",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "069085533a897be8577e",
+    "url": "/pomodoro/static/css/main.a71e89dd.chunk.css"
+  },
+  {
+    "revision": "8bdcd03b38a1a2937cf3",
+    "url": "/pomodoro/static/js/2.8ec7c6f4.chunk.js"
+  },
+  {
+    "revision": "069085533a897be8577e",
+    "url": "/pomodoro/static/js/main.45f6bdff.chunk.js"
+  },
+  {
+    "revision": "8d8194bde761299ff809",
+    "url": "/pomodoro/static/js/runtime~main.5f1f8454.js"
+  }
+]);
